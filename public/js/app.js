55a98a47444cb22ba94826e345eb1e28c12eae1a
Khaliq Gant <khaliqgant@gmail.com>
2017-10-16 22:17:50 +0200

    ecs launch configuration

--- public/js/app.js
+++ public/js/app.js
@@ -52,11 +52,14 @@
 /* 1 */
 /***/ (function(module, exports) {
 
-	var LastFmButton = document.getElementsByClassName('js-lastfm-login')[0];
-	LastFmButton.addEventListener('click', function () {
-	    // replace the button with an input area so the user can put in their user name
-	    console.log('clicked');
-	});
+	var LastFmButtons = document.getElementsByClassName('js-lastfm-login');
+	if (LastFmButtons.length > 0) {
+	    var LastFmButton = document.getElementsByClassName('js-lastfm-login')[0];
+	    LastFmButton.addEventListener('click', function () {
+	        // replace the button with an input area so the user can put in their user name
+	        console.log('clicked');
+	    });
+	}
 
 
 /***/ }),
--- public/js/listeners.ts
+++ public/js/listeners.ts
@@ -1,8 +1,11 @@
-const LastFmButton: Element = document.getElementsByClassName('js-lastfm-login')[0];
+const LastFmButtons: HTMLCollectionOf<Element> = document.getElementsByClassName('js-lastfm-login');
 
-LastFmButton.addEventListener('click', function () {
+if (LastFmButtons.length > 0) {
+    const LastFmButton: Element = document.getElementsByClassName('js-lastfm-login')[0];
+    LastFmButton.addEventListener('click', function () {
 
-    // replace the button with an input area so the user can put in their user name
-    console.log('clicked');
+        // replace the button with an input area so the user can put in their user name
+        console.log('clicked');
 
-});
+    });
+}
