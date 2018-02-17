8eee242418778fadb4dd97c19f70b672db5e53a1
Khaliq Gant <khaliqgant@gmail.com>
2018-02-17 12:25:45 +0100

    rm js files

--- public/js/listeners.js
+++
@@ -1,8 +0,0 @@
-var LastFmButtons = document.getElementsByClassName('js-lastfm-login');
-if (LastFmButtons.length > 0) {
-    var LastFmButton = document.getElementsByClassName('js-lastfm-login')[0];
-    LastFmButton.addEventListener('click', function () {
-        // replace the button with an input area so the user can put in their user name
-        console.log('clicked');
-    });
-}
